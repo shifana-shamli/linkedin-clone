993d797ab6c954d4b3baedb635fdaf2c2ae7794f
shifana-shamli <110965622+shifana-shamli@users.noreply.github.com>
2022-11-16 11:52:01 +0530

    Add files via upload

---
+++ lib/model/company_model.dart
@@ -0,0 +1,11 @@
+class CompanyModel {
+  final String profileUrl;
+  final String name;
+  final String description;
+
+  CompanyModel(
+      {
+        this.profileUrl,
+      this.name,
+      this.description});
+}
---
+++ lib/screen/pages/mobile/widget/common.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:linkedin_clone/screen/widgets/style/theme.dart';
+
+class SingleNavBarButton extends StatelessWidget {
+  final IconData icon;
+  final String name;
+  final bool selectedButton;
+
+  SingleNavBarButton(
+      {this.icon, this.name, this.selectedButton = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 28,
+          width: 28,
+          child: Icon(
+            icon,
+            color: selectedButton ? blueColor : Colors.black87,
+          ),
+        ),
+        Text(
+          name,
+          style: TextStyle(
+            fontSize: 14,
+            color: selectedButton ? blueColor : Colors.black87,
+          ),
+        )
+      ],
+    );
+  }
+}
